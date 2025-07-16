165a60076a6cc9c872e670a689fed64dc0225aaf
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2025-07-15 20:21:07 -0500

    first commit

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,9 +1,8 @@
 // src/lib/axiosClient.ts
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080', // URL base de tu API
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
